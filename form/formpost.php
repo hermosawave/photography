963966123b1301bef7692bbf457e30e8b308eac6
Daniel Sofer <1056702+hermosawave@users.noreply.github.com>
2020-12-25 19:04:28 +0900

    Using $mail

--- form/formpost.php
+++ form/formpost.php
@@ -1,5 +1,20 @@
 
 <?php 
+$email = $_POST("email");
+$message = 'This message was sent from ' . $_POST("name") . ' at ' . $email;
+$message = $message . '
+
+' . $_POST("comment");
+
+
+<?php
+$to      = $email;
+$subject = 'Mailform Test';
+$headers = 'From: hostmaster@hermosawaveinternet.com' . "\r\n" .
+    'Reply-To: hostmaster@hermosawaveinternet.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
 
 header('Location: formthanks.html?name=' . $_POST["name"]);
 exit();
