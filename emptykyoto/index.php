5cf199135f77607252d0f5e92719d9ba0c3a65d5
Daniel Sofer <1056702+hermosawave@users.noreply.github.com>
2023-05-29 21:56:26 +0900

    Disabled Kickstarter links

--- emptykyoto/index.php
+++ emptykyoto/index.php
@@ -2,10 +2,12 @@
 	$goto = $_GET["s"];
 switch ($goto) {
 	case "qr":
-		header("location:https://www.kickstarter.com/projects/hermosawave/1909053314");
+		header("location:welcome.html");
+		// header("location:https://www.kickstarter.com/projects/hermosawave/1909053314");
 		break;
 	case "url":
-		header("location:https://www.kickstarter.com/projects/hermosawave/1909053314");
+		header("location:welcome.html");
+		// header("location:https://www.kickstarter.com/projects/hermosawave/1909053314");
 		break;
 	default:
 		header("location:welcome.html");
