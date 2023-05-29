e2d51a9ca3584ccf4181019c74d19840c47c39e5
Daniel Sofer <1056702+hermosawave@users.noreply.github.com>
2023-05-29 11:42:02 +0900

    relearning php

--- emptykyoto/index.php
+++ emptykyoto/index.php
@@ -1,7 +1,17 @@
 <?php 
-	if ($_GET=="qr") elseif ($_GET=="url") {
+	$goto = $_GET["s"];
+switch ($goto) {
+	case "qr":
 		header("location:https://www.kickstarter.com/projects/hermosawave/1909053314")
-	} else {
+		break;
+	case "url":
+		break;
+	default:
 		header("location:welcome.html")
-	}
+
+}
+
+	elseif {$_GET=="url"}
+	} else {
+	} endif
 ?>
