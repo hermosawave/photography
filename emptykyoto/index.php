b0b21681e9f9caed66458ebe7db00fd72794e50d
Daniel Sofer <1056702+hermosawave@users.noreply.github.com>
2023-05-29 11:44:00 +0900

    php

--- emptykyoto/index.php
+++ emptykyoto/index.php
@@ -2,16 +2,12 @@
 	$goto = $_GET["s"];
 switch ($goto) {
 	case "qr":
-		header("location:https://www.kickstarter.com/projects/hermosawave/1909053314")
+		header("location:https://www.kickstarter.com/projects/hermosawave/1909053314");
 		break;
 	case "url":
+		header("location:https://www.kickstarter.com/projects/hermosawave/1909053314");
 		break;
 	default:
-		header("location:welcome.html")
-
+		header("location:welcome.html");
 }
-
-	elseif {$_GET=="url"}
-	} else {
-	} endif
 ?>
