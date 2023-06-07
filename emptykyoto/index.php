7b795f660711172b9d4a5efb602c6741418c25ba
Daniel Sofer <1056702+hermosawave@users.noreply.github.com>
2023-06-07 18:35:37 +0900

    PotD Jun7 Part 1

--- emptykyoto/index.php
+++ emptykyoto/index.php
@@ -3,11 +3,11 @@
 switch ($goto) {
 	case "qr":
 		header("location:welcome.html");
-		// header("location:https://www.kickstarter.com/projects/hermosawave/empty-kyoto");
+		// header("location:https://www.kickstarter.com/projects/hermosawave/empty-kyoto?ref=dg0o4m");
 		break;
 	case "url":
 		header("location:welcome.html");
-		// header("location:https://www.kickstarter.com/projects/hermosawave/empty-kyoto");
+		// header("location:https://www.kickstarter.com/projects/hermosawave/empty-kyoto?ref=dg0o4m");
 		break;
 	default:
 		header("location:welcome.html");
