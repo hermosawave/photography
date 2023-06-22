a0cb776c4fe2e72d2d66063fe8331134e470cec2
Daniel Sofer <1056702+hermosawave@users.noreply.github.com>
2023-06-21 22:02:08 -0700

    Updated Empty Kyoto page

--- emptykyoto/index.php
+++ emptykyoto/index.php
@@ -2,17 +2,17 @@
 	$goto = $_GET["s"];
 switch ($goto) {
 	case "qr":  // from booklet QR code //
-		header("location:welcome.html");
-		// header("location:https://www.kickstarter.com/projects/hermosawave/empty-kyoto?ref=dg0o4m");
+		//header("location:welcome.html");
+		header("location:https://www.kickstarter.com/projects/hermosawave/empty-kyoto?ref=dg0o4m");
 		break;
 	case "url": // from booklet url //
-		header("location:welcome.html");
-		// header("location:https://www.kickstarter.com/projects/hermosawave/empty-kyoto?ref=dg0o4m");
+		// header("location:welcome.html");
+		header("location:https://www.kickstarter.com/projects/hermosawave/empty-kyoto?ref=dg0o4m");
 		break;
 		
 	case "ghost": // from emptykyoto blog //
-		header("location:welcome.html");
-		// header("location:https://www.kickstarter.com/projects/hermosawave/empty-kyoto?ref=atvp9q");
+		// header("location:welcome.html");
+		header("location:https://www.kickstarter.com/projects/hermosawave/empty-kyoto?ref=atvp9q");
 		break;
 	default:
 		header("location:welcome.html");
