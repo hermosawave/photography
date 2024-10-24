ee077cd30b54a00fd2df1cb2132a186af1a64225
Daniel Sofer <1056702+hermosawave@users.noreply.github.com>
2024-10-24 20:44:35 +0900

    PotD Oct 24

--- emptykyoto/index.php
+++ emptykyoto/index.php
@@ -1,7 +1,8 @@
 <?php 
 	$goto = $_GET["s"];
 switch ($goto) {
-	// case "calendar":
+	// case "calendar" (QR code on the back of the 2025 calendar):
+	//case "bookpromo" (QR code on the inside flap of gifted books):
 	default:
 		header("location:home.html");
 }
