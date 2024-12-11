c95bac80149647f1c8531e837d8624777c61f5d8
Daniel Sofer <1056702+hermosawave@users.noreply.github.com>
2024-12-10 19:30:20 -0800

    edits

--- store/index.php
+++ store/index.php
@@ -99,9 +99,15 @@ font-family: ryo-gothic-plusn, sans-serif; (200, 400, 700)
      </div>
 
  <div id="frontpageright">
-         <p>
-             Picture sizes here
-            </p>
+     <p>All prints are made by the artist.</p>
+         <h3>Prints:</h3>
+         <p>5” x 7” (2L)</p>
+         <p>A4 (LTR)</p>
+         <p>13” x 19” (Super B, A3ノビ)</p>
+        <h3>Framed Prints:</h3>
+        <p>5” x 7” (2L)</p>
+        <p>A4 (LTR)</p>
+        <p>13” x 19” (Super B, A3ノビ)</p>
   </div>
        
 </div>
