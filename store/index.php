c4974edb0b6b0242f3f91802f9f670c8f28795dc
Daniel Sofer <1056702+hermosawave@users.noreply.github.com>
2024-12-25 21:36:45 -0800

    php syntax

--- store/index.php
+++ store/index.php
@@ -95,8 +95,8 @@ font-family: ryo-gothic-plusn, sans-serif; (200, 400, 700)
             <h2><?php echo $_GET['title']; ?></h2>
       <p class="en">
                 <?php 
-                $photodate = $_GET['date'];
-                // $formatteddate = $photodate("F j, Y, g:i a");  
+                $photodate == $_GET['date'];
+                // $formatteddate == $photodate("F j, Y, g:i a");  
                 echo $photodate; ?>  •  Picturecode <?php echo $_GET['picturecode']; ?>
                 <?php //  ?>
             </p>
